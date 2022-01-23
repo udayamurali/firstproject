06d727bb34d176cc8696fc26fbad70d9f87656e2
Pissati <874141@corpaa.aa.com>
2022-01-22 21:56:55 -0600

    second added

--- FirstProject/src/test/java/com/mu/selenium/FirstProject/FirstClass.java
+++ FirstProject/src/test/java/com/mu/selenium/FirstProject/FirstClass.java
@@ -3,7 +3,7 @@ package com.mu.selenium.FirstProject;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello world");
 	}
 
 }
